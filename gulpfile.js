82f50be95787ce61ceebd6cb6ad94c00cbe3f52c
frol <frol.mr@ya.ru>
2015-06-01 01:08:24 +0300

    Gulpfile modified

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,20 @@ var gulp = require('gulp'),
 	livereload = require('gulp-livereload'),
 	prefix = require('gulp-autoprefixer'),
 	sass = require('gulp-sass'),
-  wiredep = require('wiredep').stream;
+  wiredep = require('wiredep').stream,
+  useref = require('gulp-useref'),
+  minifyCss = require('gulp-minify-css'),
+  gulpif = require('gulp-if'),
+  clean = require('gulp-clean'),
+  uglify = require('gulp-uglify');
+
+var paths = {
+  images: ['app/images/**'],
+  fonts: ['app/fonts/**'],
+  fav: ['app/favicon.png'],
+  css: ['app/styles/css/main.css'],
+  html: ['app/*.html']
+}
 
 
 gulp.task('default', ['sass', 'watch']);
@@ -16,7 +29,6 @@ gulp.task('sass', function() {
     .pipe(prefix("last 10 version", "> 1%", "ie 8", "ie 7"))
     .pipe(gulp.dest('app/styles/css'))
 });
-
 gulp.task('watch', function(){
   livereload.listen();
   gulp.watch('app/styles/sass/*.sass', ['sass']).on('change', livereload.changed);
@@ -29,4 +41,34 @@ gulp.task('bower', function () {
       directory: "bower_components"
     }))
     .pipe(gulp.dest('./app'));
+});
+
+gulp.task('clean', function(){
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('production', ['images', 'fonts', 'fav', 'css','html'])
+
+
+gulp.task('images', function(){
+  gulp.src(paths.images)
+  .pipe(gulp.dest('dist/images/'));
+});
+gulp.task('fonts', function(){
+  gulp.src(paths.fonts)
+  .pipe(gulp.dest('dist/fonts/'));
+});
+gulp.task('fav', function(){
+  gulp.src(paths.fav)
+  .pipe(gulp.dest('dist/'));
+});
+gulp.task('html', function(){
+  gulp.src(paths.html)
+  .pipe(gulp.dest('dist/'));
+});
+gulp.task('css', function(){
+  gulp.src(paths.css)
+  .pipe(minifyCss({compatibility: 'ie8'}))
+  .pipe(gulp.dest('dist/styles/css/'));
 });
