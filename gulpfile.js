5087a7e57ad7513dce48bcd9244feef6c963d579
frol <frol.mr@ya.ru>
2015-06-03 00:22:17 +0300

    Mistakes fixed. some refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -10,25 +10,62 @@ var gulp = require('gulp'),
   minifyCss = require('gulp-minify-css'),
   gulpif = require('gulp-if'),
   clean = require('gulp-clean'),
+  htmlmin = require('gulp-htmlmin'),
   uglify = require('gulp-uglify');
 
 var paths = {
-  images: ['app/images/**'],
+  images: ['app/images/*.*'],
   fonts: ['app/fonts/**'],
   fav: ['app/favicon.png'],
-  css: ['app/styles/css/main.css'],
+  sass: ['app/styles/sass/main.sass'],
   html: ['app/*.html']
 }
 
+gulp.task('default', ['bower', 'sass', 'html', 'images', 'fav', 'fonts', 'watch']);
+gulp.task('clean', function(){
+  gulp.src('dist', {
+    read: false
+  })
+    .pipe(clean());
+});
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('html', function() {
+  gulp.src(paths.html)
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('./dist'))
+})
 
 gulp.task('sass', function() {
-  gulp.src('app/styles/sass/main.sass')
+  gulp.src(paths.sass)
     .pipe(sass())
-    .pipe(prefix("last 10 version", "> 1%", "ie 8", "ie 7"))
-    .pipe(gulp.dest('app/styles/css'))
+    .pipe(prefix("last 10 version", "> 1%", "ie 8"))
+    .pipe(minifyCss({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('./dist/styles'))
 });
+
+gulp.task('bower', function () {
+  gulp.src('./app/index.html')
+    .pipe(wiredep({
+      directory: "bower_components"
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('images', function(){
+  gulp.src(paths.images)
+  .pipe(gulp.dest('dist/images/'));
+});
+gulp.task('fav', function(){
+  gulp.src(paths.fav)
+  .pipe(gulp.dest('dist/'));
+});
+gulp.task('fonts', function(){
+  gulp.src(paths.fonts)
+  .pipe(gulp.dest('dist/fonts/')); 
+});
+
 gulp.task('watch', function(){
   livereload.listen();
   gulp.watch('app/styles/sass/*.sass', ['sass']).on('change', livereload.changed);
@@ -41,34 +78,4 @@ gulp.task('bower', function () {
       directory: "bower_components"
     }))
     .pipe(gulp.dest('./app'));
-});
-
-gulp.task('clean', function(){
-  return gulp.src('dist', {read: false})
-    .pipe(clean());
-});
-
-gulp.task('production', ['images', 'fonts', 'fav', 'css','html'])
-
-
-gulp.task('images', function(){
-  gulp.src(paths.images)
-  .pipe(gulp.dest('dist/images/'));
-});
-gulp.task('fonts', function(){
-  gulp.src(paths.fonts)
-  .pipe(gulp.dest('dist/fonts/'));
-});
-gulp.task('fav', function(){
-  gulp.src(paths.fav)
-  .pipe(gulp.dest('dist/'));
-});
-gulp.task('html', function(){
-  gulp.src(paths.html)
-  .pipe(gulp.dest('dist/'));
-});
-gulp.task('css', function(){
-  gulp.src(paths.css)
-  .pipe(minifyCss({compatibility: 'ie8'}))
-  .pipe(gulp.dest('dist/styles/css/'));
 });
