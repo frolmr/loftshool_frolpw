5b31ef176c5e69f50ede76b1f3a605041c404027
frol <frol.mr@ya.ru>
2015-06-22 16:16:24 +0300

    still faiting with ie8

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,7 +19,7 @@ function handleFileInput(){
 }
 
 function showModal(event){
-	event.preventDefault ? event.preventDefault() : event.returnValue = false;
+	event.preventDefault ? event.preventDefault() : event.returnValue;
 	$()
 	$('.overlay').fadeIn(400, function() {
 		$('#modal').show().animate({opacity: 1}, 400);
@@ -96,7 +96,7 @@ function removeTooltiped(field) {
 
 function pushSubmit(form){
 	$('form').on('submit', function(event) {
-		event.preventDefault ? event.preventDefault() : event.returnValue = false;
+		event.preventDefault ? event.preventDefault() : event.returnValue;
 		tooltipEmptyField(form);
 	});
 }
